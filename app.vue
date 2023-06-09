ea03f5b0a713d09a543dc7e0894bc563027a0463
mofutaro <jnakadev@gmail.com>
2023-06-09 21:29:49 +0900

    feat: パスワード生成機能を追加

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+const charset = '23456789abcdefghjkmnpqrtuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+const count = ref('');
+const copyToClipBoard = () => {};
+const changePassword = () => {
+  let p: string = '';
+  for (let i = 0; i < 10; i++) {
+    const rand: number = Math.floor(Math.random() * charset.length);
+    p += charset[rand];
+  }
+  count.value = p;
+};
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>ReadablePass</h1>
+    <div id="pass-container">{{ count }}</div>
+    <button @click="changePassword">Change</button>
   </div>
 </template>
